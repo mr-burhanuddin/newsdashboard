dfd67098082fbb5d56e9de12dd51d070c25e352b
mr-burhanuddin <bjpresswala@gmail.com>
2021-06-13 12:39:58 +0530

    pagination added api added

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -2,27 +2,20 @@ import React from "react";
 import "./Card.css";
 import ClearIcon from "@material-ui/icons/Clear";
 
-function Card() {
+function Card({ id, key, title, date, summary, link }) {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
   return (
     <div className="card">
       <div className="card__info">
         <ClearIcon className="card__cancelButton" />
         <div className="card__content">
-          <h2>Title</h2>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda,
-            nobis illo esse tempora maiores earum praesentium exercitationem
-            quis corrupti est ad vel! Vitae repellat minima est eius sit ut
-            nihil!
-          </span>
-          <p className="card__Date">Date</p>
+          <h4>{title}</h4>
+          <span>{truncate(summary, 150)}</span>
+          <p className="card__Date">{date}</p>
         </div>
       </div>
-      <img
-        className="card__img"
-        src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
-        alt=""
-      />
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import news from "../../api/news";
 import Card from "./Card";
 import "./Main.css";
+import ReactPaginate from "react-paginate";
 
 function Main() {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const postsPerPage = 6;
+
+  useEffect(() => {
+    const fetchRes = async () => {
+      const res = await news.get();
+      setPosts(res.data.data);
+    };
+    fetchRes();
+  }, []);
+  console.log("i am data", posts);
+
+  //Get Current Posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  // const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(
+    indexOfLastPost,
+    indexOfLastPost + postsPerPage
+  );
+  const pageNumber = Math.ceil(posts.length / postsPerPage);
+
+  const changePage = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+  console.log(currentPage);
+
   return (
     <div className="main">
       <div className="main__cardContainer">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {currentPosts.map(({ id, link, published, summary, title }) => (
+          <Card
+            title={title}
+            id={id}
+            key={id}
+            link={link}
+            date={published}
+            summary={summary}
+          />
+        ))}
       </div>
+      <>
+        <ReactPaginate
+          previousLabel={"<<"}
+          nextLabel={">>"}
+          pageCount={pageNumber}
+          onPageChange={changePage}
+          containerClassName={"paginationBttns"}
+          previousClassName={"previousBttn"}
+          nextClassName={"nextBttn"}
+          activeClassName={"paginationActive"}
+        />
+      </>
     </div>
   );
 }
